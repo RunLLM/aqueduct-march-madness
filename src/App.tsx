512f9974fa34388f55918e261d2ca0b864f7ae28
Andre Giron <andre@spiralai.co>
2023-03-03 16:22:51 -0800

    Sets up initial SingleEliminationMatch component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { SingleEliminationBracket, Match, SVGViewer } from '@g-loot/react-tournament-brackets';
+
+type SingleEliminationMatch = {
+  id: number;
+  name: string;
+  nextMatchId: string | null;
+  tournamentRoundText: string;
+  startTime: string;
+  state: "DONE" | "NO_SHOW" | "WALK_OVER" | "NO_PARTY" | "DONE" | "SCORE_DONE"
+  participants: Participant[];
+}
+
+type Participant = {
+  id: string;
+  resultText: string | null;
+  isWinner: boolean;
+  status: 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null;
+  name: string;
+}
+
+const matches: SingleEliminationMatch[] = [
+  {
+    "id": 260005,
+    "name": "Final - Match",
+    "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
+    "tournamentRoundText": "4", // Text for Round Header
+    "startTime": "2021-05-30",
+    "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
+    "participants": [
+      {
+        "id": "c016cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc", // Unique identifier of any kind
+        "resultText": "WON", // Any string works
+        "isWinner": false,
+        "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
+        "name": "giacomo123"
+      },
+      {
+        "id": "9ea9ce1a-4794-4553-856c-9a3620c0531b",
+        "resultText": null,
+        "isWinner": true,
+        "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY'
+        "name": "Ant"
+      }
+    ]
+  }
+]
+
+
+export const SingleElimination = () => (
+  <SingleEliminationBracket
+    matches={matches}
+    matchComponent={Match}
+    svgWrapper={({ children, ...props }) => (
+      <SVGViewer width={500} height={500} {...props}>
+        {children}
+      </SVGViewer>
+    )}
+  />
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SingleElimination />
     </div>
   )
+
+
+  //const [count, setCount] = useState(0)
+
+  // return (
+  //   <div className="App">
+  //     <div>
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src="/vite.svg" className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://reactjs.org" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </div>
+  // )
 }
 
 export default App
